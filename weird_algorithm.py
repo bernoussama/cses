ca05f1c691af3dc4972ec5dbc8af32a6f11c3e25
bernoussama <oussama.bernou@outlook.com>
2024-11-24 17:16:36 +0100

    weird_algorithm
    in python

---
+++ weird_algorithm.py
@@ -0,0 +1,18 @@
+def even(n: int):
+    return n % 2 == 0
+
+
+def weird_algorithm(n: int):
+    print(n, end=" ")
+    if n == 1:
+        print()
+        return
+    if even(n):
+        weird_algorithm(round(n / 2))
+    elif not even(n):
+        weird_algorithm(n * 3 + 1)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    weird_algorithm(n)
